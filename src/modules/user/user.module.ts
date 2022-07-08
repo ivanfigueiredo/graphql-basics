8c3dee2da401fea300283ac68cb18dfbe8a3204e
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-07-08 08:20:12 -0300

    feat: create user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { TesteResolver } from './teste.input';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserRepository } from './infra/db-mongo/use.repository';
+import { Cat, CatSchema } from './infra/db-mongo/user.model';
+import { TesteResolver } from './infra/graphql/user.resolver';
+import { CreateUserUseCase } from './use-cases/create-user/create-user.use-case';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [],
-  providers: [TesteResolver]
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  providers: [
+    TesteResolver,
+    CreateUserUseCase,
+    {
+			provide: 'UserRepository',
+			useClass: UserRepository
+		},
+    UserService
+  ],
+  exports: [UserService]
 })
 export class UserModule {}
